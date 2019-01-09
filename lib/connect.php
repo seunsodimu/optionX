386f8bb4b188951a46f0fbdb6ea246b76f5dd623
Oluwaseun Sodimu <seun.sodimu@yahoo.com>
2019-01-09 09:21:48 -0600

    Update connect.php

--- lib/connect.php
+++ lib/connect.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "godzilla", "carmax");
+$mysqli = new mysqli("localhost", "optionex_admin", "4pata34nasa25na!", "optionex_db");
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
